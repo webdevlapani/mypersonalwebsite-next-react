f6e0c2057e9be8b4ff912a07398f88e94e47f35a
PratikDabhi <pratik.dabhi888@gmail.com>
2020-04-30 19:18:07 +0530

    completed my website

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Suhag Lapani'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Layout from '../../components/layout'
+import {getAllPostIds, getPostData} from '../../lib/posts'
+import Date from "../../components/date";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params}) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+import utilStyles from '../../styles/utils.module.css'
+import Head from "next/dist/next-server/lib/head";
+import React from "react";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
